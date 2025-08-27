b84f32d9b2ebbda0eddcb00797f545a99761f089
queenxiquec <phamthingocquynh1409@gmail.com>
2025-08-26 22:37:19 -0700

    Add files via upload

---
+++ Dataset.h
@@ -0,0 +1,107 @@
+#pragma once
+#include<iostream>
+#include <cstdlib> 
+#include <ctime> 
+using namespace std;
+class Dataset
+{
+private:
+    int* array;
+    int size;
+    int capacity;
+    bool isSample;  // true = Sample, false = Population
+
+    void resize()
+    {
+        int newCapacity = capacity + 1;
+        int* newArr = new int[newCapacity];
+        for (int i = 0; i < size; i++) newArr[i] = array[i];
+        delete[] array;
+        array = newArr;
+        capacity = newCapacity;
+    }
+
+    // keeps array sorted after adding one element
+    void insertOne(int newValue)
+    {
+        if (size == capacity) resize();
+        array[size] = newValue;
+        size++;
+        int temp = array[size - 1];
+        int j = size - 2;
+        while (j >= 0 && array[j] > temp)
+        {
+            array[j + 1] = array[j];
+            j--;
+        }
+        array[j + 1] = temp;
+    }
+
+public:
+    // Constructor
+    Dataset(int initCap = 1)  // starts with 1 slot minimum
+    {
+        if (initCap < 1) initCap = 1;
+        size = 0;
+        capacity = initCap;
+        array = new int[capacity];
+        srand(time(0)); // seed random numbers
+    }
+
+    // Destructor
+    ~Dataset()
+    {
+        delete[] array;
+    }
+
+    // NEW: insert exactly one specified value
+    void insertSort(int value) 
+    {
+        insertOne(value);
+    }
+
+    // existing: insert 'count' random values in [0..maxValue]
+    void insertSort(int count, int maxValue)
+    {
+        for (int i = 0; i < count; i++) 
+        {
+            int rnd = rand() % (maxValue + 1);
+            insertOne(rnd);
+        }
+    }
+
+    void display() const
+    {
+        if (size == 0)
+        {
+            cout << "(empty)\n";
+            return;
+        }
+        cout << "\n\n\t";
+        for (int i = 0; i < size; i++)
+        {
+            cout << array[i] << (i == size - 1 ? '\n' : ' ');
+        }
+    }
+
+    // Getter
+    int getSize() const { return size; }
+    int getCapacity() const { return capacity; }
+    
+
+    void setSample(bool sample) { isSample = sample; }
+    string getType() const
+    {
+        if (isSample)
+        {
+            return "Sample";
+        }
+        else
+        {
+            return "Population";
+        }
+    }
+
+
+};
+
--- main.cpp
+++ main.cpp
@@ -1,79 +1,205 @@
-﻿#include <iostream>
-#include <limits>
+#include<iostream>
+#include <iomanip>
+#include"input.h"
+#include<fstream>
+#include"Dataset.h"
+
 using namespace std;
 
-int inputInteger(const char* prompt) {
-    int x;
-    while (true) {
-        cout << prompt;
-        if (cin >> x) return x;
-        cout << "Invalid input. Please enter an integer.\n";
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    }
-}
+void showMenu();
+void showIntro();
+void insertMenu();
+Dataset ds;
 
-void showMainMenu() {
-    cout << "\nDescriptive Statistics Calculator Main Menu\n";
-    cout << "════════════════════════════════════════════════════════════════════════════════\n";
-    cout << " 0. Exit\n";
-    cout << " 1. Configure Dataset to Sample or Polulation\n";
-    cout << " 2. Insert sort value(s) to the Dataset\n";
-    cout << " 3. Delete value(s) from the Dataset\n";
-    cout << "───────────────────────────────────────────────────────────────────────────────\n";
-    cout << " A. Find Minimum                   N. Find Outliers\n";
-    cout << " B. Find Maximum                   O. Find Sum of Squares\n";
-    cout << " C. Find Range                     P. Find Mean Absolute Deviation\n";
-    cout << " D. Find Size                      Q. Find Root Mean Square\n";
-    cout << " E. Find Sum                       R. Find Standard Error of Mean\n";
-    cout << " F. Find Mean                      S. Find Skewness\n";
-    cout << " G. Find Median                    T. Find Kurtosis\n";
-    cout << " H. Find Mode(s)                   U. Find Kurtosis Excess\n";
-    cout << " I. Find Standard Deviation        V. Find Coefficient of Variation\n";
-    cout << " J. Find Variance                  W. Find Relative Standard Deviation\n";
-    cout << " K. Find Midrange                  X. Display Frequency Table\n";
-    cout << " L. Find Quartiles                 Y. Display ALL statical results\n";
-    cout << " M. Find Interquartile Range       Z. Output ALL statical results to text file\n";
-    cout << "════════════════════════════════════════════════════════════════════════════════\n";
-    cout << "Option: ";
-}
+int main()
+{
+    showIntro();
 
-int main() {
-    char choice;
-    while (true) {
-        showMainMenu();
-        cin >> choice;
+    do
+    {
+        system("cls");
 
-        // unify case (accept lower/upper)
-        if (choice >= 'a' && choice <= 'z')
-            choice = choice - 'a' + 'A';
+        cout << "\n\tAddress of Dynamic array: ";
+        cout << "\n\tDataset: (" << ds.getType() << ")\n\t";
+        ds.display();
+        showMenu();
 
-        switch (choice) {
-        case '0':
-            cout << "Exiting program...\n";
-            return 0;
+       
 
+        switch (toupper(inputChar("\n\tOption: ")))
+        {
+        case '0': exit(0);
         case '1':
-            cout << "[Configure Dataset]\n";
-            break;
-        case '2':
-            cout << "[Insert sort value(s)]\n";
-            break;
-        case '3':
-            cout << "[Delete value(s)]\n";
+        {
+            system("cls");
+
+            cout << "\n\n\tIn statistics, Population refers to the entire group of data";
+            cout << "\n\tpoints that a study is interested in, while a Sample is a";
+            cout << "\n\tsubset of that population that is actually used in the study.";
+
+            cout << "\n\n\tConfigure Dataset Menu";
+            cout << "\n\t" << string(80, char(205));
+            cout << "\n\t\tA. sample";
+            cout << "\n\t\tB. population";
+            cout << "\n\t" << string(80, char(196));
+            cout << "\n\t\tR. return";
+            cout << "\n\t" << string(80, char(205));
+
+            switch (toupper(inputChar("\n\t\tOption: ")))
+            {
+            case 'A':
+            {
+                cout << "\n\tData Set configured for Sample";
+                ds.setSample(true);
+            }
+                break;
+
+            case 'B':
+            {
+                cout << "\n\tData Set configured for Population";
+                ds.setSample(false);
+            }
+                break;
+            case 'R': break;
+            }
+
+
+
+        }
+        break;
+        case '2': 
+        {
+            insertMenu();
+        }
+
             break;
 
-        case 'A':
-            cout << "[Find Minimum]\n";
-            break;
-        case 'B':
-            cout << "[Find Maximum]\n";
-            break;
-        case 'C':
-            cout << "[Find Range]\n";
-            break;
-        default:
-            cout << "ERROR: Invalid option.\n";
+        default: cout << "\t\tERROR - Invalid option."; break;
         }
-    }
+
+        cout << "\n";
+        system("pause");
+
+    } while (true);
+
+    return EXIT_SUCCESS;
+}
+
+void showMenu()
+{
+    
+    cout << "\n\tERROR: Data Set requires at least 2 values.";
+
+    cout << "\n\n\tDescriptive Statistics Calculator Main Menu\n";
+    cout << "\n\t" << string(85, char(205));
+    cout << "\n\t0. Exit";
+    cout << "\n\t1. Configure Dataset to Sample or Polulation";
+    cout << "\n\t2. Insert sort value(s) to the Dataset";
+    cout << "\n\t3. Delete value(s) from the Dataset";
+    cout << "\n\t" << string(85, char(196));
+    cout << "\n\tA. Find Minimum \t\t\tN. Find Outliers";
+    cout << "\n\tB. Find Maximum \t\t\tO. Find Sum of Squares";
+    cout << "\n\tC. Find Range \t\t\t\tP. Find Mean Absolute Deviation";
+    cout << "\n\tD. Find Size \t\t\t\tQ. Find Root Mean Square";
+    cout << "\n\tE. Find Sum \t\t\t\tR. Find Standard Error of Mean";
+    cout << "\n\tF. Find Mean \t\t\t\tS. Find Skewness";
+    cout << "\n\tG. Find Median \t\t\t\tT. Find Kurtosis";
+    cout << "\n\tH. Find Mode(s) \t\t\tU. Find Kurtosis Excess";
+    cout << "\n\tI. Find Standard Deviation \t\tV. Find Coefficient of Variation";
+    cout << "\n\tJ. Find Variance \t\t\tW. Find Relative Standard Deviation";
+    cout << "\n\tK. Find Midrange \t\t\tX. Display Frequency Table";
+    cout << "\n\tL. Find Quartiles \t\t\tY. Display ALL statical results";
+    cout << "\n\tM. Find Interquartile Range \t\tZ. Output ALL statical results to text file";
+    cout << "\n\t" << string(85, char(205)) << "\n";
+   
+}
+
+void showIntro()
+{
+    cout << "\n\tWhat are Descriptive Statistics?";
+    cout << "\n\n\tDescriptive statistics summarize certain aspects of a data set (Sample or Population)\n\tusing numeric calculations.";
+    cout << "\n\n\thttps://www.calculatorsoup.com/calculators/statistics/descriptivestatistics.php.";
+    cout << "\n\nPress any key to continue . . . ";
+    cin.get();
+
+    system("cls");
+}
+
+void insertMenu()
+{
+    do
+    {
+        system("cls");
+        cout << "\n\n\tInsert (sort) Dataset Menu";
+        cout << "\n\t" << string(80, char(205));
+        cout << "\n\t\tA. insert a value";
+        cout << "\n\t\tB. insert a specified number of random values";
+        cout << "\n\t\tC. read data from file and insert values";
+        cout << "\n\t" << string(80, char(196));
+        cout << "\n\t\tR. return";
+        cout << "\n\t" << string(80, char(205));
+
+        switch (toupper(inputChar("\n\n\t\tOption: ")))
+        {
+        case 'A':
+        {
+            int val = inputInteger("\n\n\tSpecify an integer value to be inserted: ");
+            ds.insertSort(val); 
+            cout << "\n\t" << val << " has been inserted...\n";
+            cout << "\n\t";
+            system("pause");
+            
+        }
+        break;
+        case 'B':
+        {
+            int count = inputInteger("\n\n\tSpecify a number of values to be randomly generated into the Dataset: ", 1, 1000);
+            int maxVal =  10000;
+            ds.insertSort(count, maxVal);
+            cout << "\n\tCONFIRMATION: Inserted " << count << " random values into the Dataset.\n";
+            cout << "\n\t";
+            system("pause");
+            break;
+        }
+        case 'C':
+        {
+            ifstream inputFile;
+            string filename;
+
+            cout << "\n\n\tSpecify a data text file name to read: ";
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // clear leftover input
+            getline(cin, filename);
+
+            inputFile.open(filename);
+
+            if (inputFile)
+            {
+                int value = 0;
+                int count = 0;
+
+                while (inputFile >> value)
+                {
+                    ds.insertSort(value);   // insert exact values into dataset
+                    count++;
+                }
+
+                cout << "\n\tCONFIRMATION: " << count
+                    << " element(s) have been read and inserted to the Dataset.\n\n";
+
+                inputFile.close();
+            }
+            else
+            {
+                cout << "\n\n\tERROR: File, " << filename << ", cannot be found.\n";
+            }
+
+            system("pause");
+        }
+        case 'R':
+            return;
+        break;
+        }
+
+    } while (true);
+
 }
