e340d759184468d0d91f5b130b584d33e887082a
queenxiquec <phamthingocquynh1409@gmail.com>
2025-08-27 23:34:37 -0700

    Update Dataset.h

--- Dataset.h
+++ Dataset.h
@@ -115,6 +115,120 @@ public:
     }
 
    
+bool deleteValue(int value)
+{
 
+    // Check if value exists at all
+    bool found = false;
+    for (int i = 0; i < size; i++) {
+        if (array[i] == value) {
+            found = true;
+            break;
+        }
+    }
+
+    cout << "\n\tDelete *-all elements or 1-one element found with value " << value << ": ";
+    char option = inputChar(" ");
+    switch (option)
+    {
+    case '1':
+    {
+        // delete first occurrence only
+        for (int i = 0; i < size; i++)
+        {
+            if (array[i] == value)
+            {
+                for (int j = i; j < size - 1; j++)
+                    array[j] = array[j + 1];
+                size--;
+                cout << "\n\tCONFIRMATION: One element " << value << " has been found and deleted. ";
+                return true;
+            }
+        }
+    }
+    break;
+    case '*':
+    {
+        // delete all occurrences
+        int newSize = 0;
+        for (int i = 0; i < size; i++)
+        {
+            if (array[i] != value)
+            {
+                array[newSize++] = array[i];
+            }
+        }
+        int removed = size - newSize;
+        size = newSize;
+        cout << "\n\tCONFIRMATION: " << removed << " instance() of element: " << value << " have been found and deleted.";
+        return true;
+    }
+    break;
+    default:
+        cout << "\n\tERROR: Invalid input. Must be one of '*1' character.";
+    }
+
+
+    if (!found)
+    {
+        cout << "\n\n\tERROR: No element " << value << " has been found and deleted.";
+        return false;
+    }
+}
+
+
+void deleteRange(int start, int end)
+{
+
+    // Collect deleted values (temporary storage for display)
+    int* deleted = new int[size];
+    int delCount = 0;
+
+    // Shift elements to remove values in range
+    int newSize = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if (array[i] >= start && array[i] <= end)
+        {
+            deleted[delCount++] = array[i];  // store deleted element
+        }
+        else
+        {
+            array[newSize++] = array[i];    // keep element
+        }
+    }
+
+    // Update dataset size
+    size = newSize;
+
+    // Output result
+    if (delCount > 0)
+    {
+        cout << "\n\tCONFIRMATION: Element(s): ";
+        for (int i = 0; i < delCount; i++)
+        {
+            cout << deleted[i] << (i == delCount - 1 ? "" : " ");
+        }
+        cout << " in range(" << start << ".." << end << ") have been deleted.\n";
+    }
+    else
+    {
+        cout << "\n\tNo elements found in range(" << start << ".." << end << ").\n";
+    }
+
+    delete[] deleted;
+}
+
+void deleteAllValues()
+{
+
+    delete[] array;        // free the memory
+    array = new int[1];    // reset with at least 1 slot
+    size = 0;
+    capacity = 1;
+
+    cout << "\n\tDataset has been purged of all elements.";
+}
 
 };
+
